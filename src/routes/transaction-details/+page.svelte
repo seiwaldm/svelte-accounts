9f0e220e0b2ded6fdd3641c1cf62f586b2273319
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-04-28 21:50:42 +0200

    accounts & transactions

---
+++ src/routes/transaction-details/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { supabase } from '$lib/supabase';
+	import { onMount } from 'svelte';
+
+	let transactionList = ['😘'];
+
+	async function getTransactions() {
+		let { data: transactions, error } = await supabase.from('transaction_details').select('*');
+		transactionList = transactions;
+	}
+
+	onMount(() => {
+		getTransactions();
+	});
+</script>
+
+<div>{JSON.stringify(transactionList)}</div>
