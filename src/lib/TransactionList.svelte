2dd98dff3106a9dc17f6ea78661023c743790d54
lesicl05 <80815302+lesic05@users.noreply.github.com>
2024-05-14 14:50:11 +0200

    Update TransactionList.svelte
    .

--- src/lib/TransactionList.svelte
+++ src/lib/TransactionList.svelte
@@ -1,15 +1,15 @@
 <script>
-    import {accountList} from "$lib/store"
-    const transactions = [
-        {
-        id: "12341234",
-        amount: "49",
-        sender_id: "11223344",
-        receiver_id: "44332211",
-        purpose: "Einkauf",
-        created_at: "2021-09-01"
-        }
-    ];
+	import { accountList } from '$lib/store';
+	const transactions = [
+		{
+			id: '12341234',
+			amount: '49',
+			sender_id: '11223344',
+			receiver_id: '44332211',
+			purpose: 'Einkauf',
+			created_at: '2021-09-01'
+		}
+	];
 </script>
 
 <div>{$accountList}</div>
