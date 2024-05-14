6b6cc0b497be0dafb71d21a6b2d630aab4178bfe
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-05-14 08:21:12 +0200

    shadcn

---
+++ src/routes/accounts/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { getLocalTimeZone, today } from '@internationalized/date';
+	import { RangeCalendar } from '$lib/components/ui/range-calendar/index.js';
+
+	const end = today(getLocalTimeZone());
+	const start = end.subtract({ months: 1 });
+
+	let value = {
+		start,
+		end
+	};
+</script>
+
+<RangeCalendar bind:value class="rounded-md border max-w-min  bg-slate-300" />
+
+{JSON.stringify(value)}
