e8af51d9d17fd0ba13b39973ff49ddd75b213312
Leo-css <leobra2005@gmail.com>
2024-05-14 14:47:25 +0200

    jkok

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -3,6 +3,7 @@
 	import { onMount } from 'svelte';
 
 	let transactionList = ['😘'];
+	let transaction = supabase.from('transactions').select();
 
 	async function getTransactions() {
 		let { data: transactions, error } = await supabase.from('transactions').select('*');
@@ -15,3 +16,16 @@
 </script>
 
 <div>{JSON.stringify(transactionList)}</div>
+
+{#await transaction}
+	<span class="loading loading-bars loading-lg text-primary" />
+{:then result}
+	<ul>
+		{#each result.data as transaction (transaction.id)}
+			<li>{transaction.id}</li>
+			<li>{transaction.purpose}</li>
+			<li>{transaction.amount}</li>
+			<li>{transaction.sender_id}</li>
+		{/each}
+	</ul>
+{/await}
