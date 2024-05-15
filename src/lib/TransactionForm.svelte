0d139d64d14b0001df14d4dab7cb9fcf6fe8dd3a
Zimtaepfelmafia <laemmerhofer.nikolas@gmail.com>
2024-05-15 12:20:39 +0200

    submitTransaction

--- src/lib/TransactionForm.svelte
+++ src/lib/TransactionForm.svelte
@@ -1,3 +1,32 @@
+<script>
+	let receiver = '';
+	let amount = '';
+	let purpose = '';
+
+	export let transaction = {
+		receiver: '',
+		amount: '',
+		purpose: ''
+	};
+
+	function submitTransaction() {
+		if (receiver === '' || amount === '' || purpose === '') {
+			alert('Bitte füllen Sie alle Felder aus');
+		} else if (!Number.isInteger(amount)) {
+			alert('Bitte geben Sie eine Zahl als Betrag ein');
+		} else if (amount <= 0) {
+			alert('Bitte geben Sie einen positiven Betrag ein');
+		} else {
+			transaction = {
+				receiver: receiver,
+				amount: amount,
+				purpose: purpose
+			};
+			console.log(transaction);
+		}
+	}
+</script>
+
 <!-- The button to open modal -->
 <label for="my_modal_6" class="btn">Neue Überweisung</label>
 
@@ -10,18 +39,26 @@
 			<label class="form-control w-full max-w-xs">
 				<input
 					type="text"
+					placeholder="Empfänger"
+					class="input input-bordered w-full max-w-xs"
+					bind:value={receiver}
+				/>
+				<input
+					type="number"
+					placeholder="Betrag"
+					class="input input-bordered w-full max-w-xs"
+					bind:value={amount}
+				/>
+				<input
+					type="text"
 					placeholder="Verwendungszweck"
 					class="input input-bordered w-full max-w-xs"
+					bind:value={purpose}
 				/>
-			</label>
-			<label class="swap swap-flip text-9xl">
-				<!-- this hidden checkbox controls the state -->
-				<input type="checkbox" />
-				<div class="swap-on text-center">SWP ist toll</div>
-				<div class="swap-off text-center">🧑‍💻🧑‍💻🧑‍💻🧑‍💻🧑‍💻🧑‍💻</div>
 			</label>
 		</p>
 		<div class="modal-action">
+			<button class="btn" on:click={submitTransaction}>Freigeben</button>
 			<label for="my_modal_6" class="btn">verwerfen</label>
 		</div>
 	</div>
