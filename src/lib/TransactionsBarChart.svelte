7bf7cf2f00f899ec11abd73d7efa9c39ee956f92
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-05-24 11:33:29 +0200

    Update TransactionsBarChart.svelte

--- src/lib/TransactionsBarChart.svelte
+++ src/lib/TransactionsBarChart.svelte
@@ -1,4 +1,6 @@
 <script>
+	// Bitt nur Variablen importieren, die im Store deklariert und exportiert wurden. (keine sender_id, sondern natürlich alle transactions -> ihr greift unten ja auch auf die transactions zu!!!) & der Import von "stores/" ist falsch -> in anderen Dateien wird auch nicht so importiert - bitte anschauen.
+
 	// import {sender_id} from 'stores/';
 	import { Bar } from 'svelte-chartjs';
 	import 'chart.js/auto';
@@ -15,6 +17,7 @@
 		datasets: [
 			{
 				label: 'Einnahmen und Ausgaben',
+				// oben gebt ihr zwei Labels an, das heißt, dass hier unter data auch zwei Werte stehen müssen, die die beiden Labels repräsentieren... entweder könnt ihr das mit der reduce-Methode machen (2x unabhängig voneinander - für Ausgaben und für Einnahmen), oder ihr iteriert mit einer for-Schleife über die transactions und erledigt mit einer if-Anweisung die Zurechnung zum richtigen Summenwert (Einnahme oder Ausgabe) -> unabhängig davon,, welchen Weg ihr wählt, würde ich die Berechung außerhalb der data-Konstante durchführen und nur das Ergebnis hier unter data.datasets.data eintragen
 				data: transactions.map((transaction) => {
 					if (transaction.receiver_id === user_id) {
 						return +transaction.amount; // + amount ursprünglicher Amount Einnahmen für den Benutzer
