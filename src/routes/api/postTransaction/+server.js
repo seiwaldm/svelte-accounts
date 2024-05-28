78107ffa8b7e1e3622e163842a79e154f63a8ab9
stefanieauer <80815387+stefanieauer@users.noreply.github.com>
2024-05-28 14:43:51 +0200

    0
    0

--- src/routes/api/postTransaction/+server.js
+++ src/routes/api/postTransaction/+server.js
@@ -1,35 +1,49 @@
-
 import { supabase } from '$lib/supabase';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export async function POST({ request, cookies }) {
-
 	//extract data from request and cookies:
 	const data = await request.json();
-	const session = JSON.parse(cookies.get("session"));
-	const accountList = JSON.parse(cookies.get("account_list"));
+	const session = JSON.parse(cookies.get('session'));
+	const accountList = JSON.parse(cookies.get('account_list'));
 
-	console.log("session:", session);
-	console.log("accountList:", accountList);
+	console.log('session:', session);
+	console.log('accountList:', accountList);
 
-	//TODO: check if the sender_id corresponds to an account of the logged in user:
+	//TODO: check if the sender_id corresponds to an account of the logged in user
 
+	//find the account in the accountList that corresponds to the user id of the session:
+	const account = accountList.find((account) => account.user_id === session.user.id);
 
+	if (data.sender_id !== account.id) {
+		return new Response('Invalid sender_id', { status: 400 });
+	}
 
 	//check if amount is positive:
 	if (data.amount <= 0) {
 		return new Response('Amount should be greater than 0', { status: 400 });
 	}
 
-
 	//TODO: perform insert operation and return response to the frontend:
-	const response = await fetch(PUBLIC_SUPABASE_URL + "/rest/v1/transactions", {
-		method: "POST",
-		headers: {},
-		body: {}
+
+	const response = await fetch(PUBLIC_SUPABASE_URL + '/rest/v1/transactions', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			apikey: PUBLIC_SUPABASE_ANON_KEY,
+			Authorization: 'Bearer ' + session.access_token
+		},
+		body:
+			//sent data as string:
+			JSON.stringify({
+				sender_id: data.sender_id,
+				receiver_id: data.receiver_id,
+				amount: data.amount,
+				purpose: data.purpose
+			})
 	});
 
-	return new Response("Transaction done", { status: 200 });
+	return new Response('Transaction done', { status: 200 });
 
 	//LEGACY CODE:
 	// if ($accounts.map(account => account.id).includes(dummy.sender_id)) {
@@ -58,5 +72,4 @@ export async function POST({ request, cookies }) {
 	// 		);
 	// 	return new Response(JSON.stringify(data));
 	// }
-
 }
