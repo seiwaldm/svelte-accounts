0532195c2ad0dbe03cc52dfd32296b65bce3d14c
Leo-css <leobra2005@gmail.com>
2024-05-28 13:36:14 +0200

    Create +page.svelte

---
+++ src/routes/kanidleo/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { transactions } from '$lib/store';
+	import Transaction from '$lib/Transaction.svelte';
+</script>
+
+<div>{JSON.stringify($transactions)}</div>
+<h1>transactions</h1>
+{#if $transactions}
+	{#each $transactions as transaction}
+		<Transaction
+			sender_id={transaction.sender_id}
+			receiver_id={transaction.receiver_id}
+			amount={transaction.amount}
+			purpose={transaction.purpose}
+			created_at={transaction.created_at}
+		/>
+	{/each}
+{/if}
+
+<h1>transactions</h1>
