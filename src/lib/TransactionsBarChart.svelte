eab45d0bcb2a60d1565bc4074b6fa288c8d90ce5
Martin Mayrhofer <Mayrhofer.Martin15@gmail.com>
2024-05-31 17:19:13 +0200

    Chart zufrieden

--- src/lib/TransactionsBarChart.svelte
+++ src/lib/TransactionsBarChart.svelte
@@ -2,56 +2,42 @@
 	import { accounts, accountList, transactions } from '$lib/store.js';
 	import { Bar } from 'svelte-chartjs';
 	import 'chart.js/auto';
-
+  
 	const income = $transactions.reduce((total, transaction) => {
-		if (transaction.receiver_id === $accounts[0].id) {
-			return total + transaction.amount;
-		} else {
-			return total;
-		}
+	  if (transaction.receiver_id === $accounts[0].id) {
+		return total + transaction.amount;
+	  } else {
+		return total;
+	  }
 	}, 0);
-
+  
 	const expenses = $transactions.reduce((total, transaction) => {
-		if (transaction.sender_id === $accounts[0].id) {
-			return total + transaction.amount;
-		} else {
-			return total;
-		}
+	  if (transaction.sender_id === $accounts[0].id) {
+		return total + transaction.amount;
+	  } else {
+		return total;
+	  }
 	}, 0);
-
-	const data1 = {
-  labels: ['Einnahmen'],
-  datasets: [
-    {
-      label: 'Einnahmen',
-      data: [income],
-      backgroundColor: ['#36A2EB'], // Blau
-      borderColor: ['rgb(159, 226, 191)'],
-      borderWidth: 2
-    }]};
-
-const data2 = {
-	  labels: ['Ausgaben'],
+  
+	const data = {
+	  labels: ['Chash Flow'],
 	  datasets: [
-    {
-      label: 'Ausgaben',
-      data: [expenses],
-      backgroundColor: ['#FF6384'], // Pink
-      borderColor: ['rgb(222, 49, 99)'],
-      borderWidth: 2
-    }
-  ]
-};
-	// 	datasets: [
-	// 		{
-	// 			label: 'Einnahmen und Ausgaben',
-	// 			data: [income, expenses],
-	// 			backgroundColor: ['#36A2EB', '#FF6384'],
-	// 			borderColor: ['rgb(159, 226, 191)', 'rgb(222, 49, 99)'],
-	// 			borderWidth: 2
-	// 		}
-	// 	]
-	// };
-</script>
-<Bar {data1} />
-<!-- <Bar {data1||data2} /> -->
+		{
+		  label: 'Einnahmen',
+		  data: [income],
+		  backgroundColor: ['rgba(58, 238, 106, 0.4)'], // Blau
+		  borderColor: ['rgba(58, 238, 106, 0.7)'],
+		  borderWidth: 2
+		},
+		{
+		  label: 'Ausgaben',
+		  data: [expenses],
+		  backgroundColor: ['rgba(238, 28, 28,0.4)'], // Pink
+		  borderColor: ['rgba(238, 28, 28, 0.7)'],
+		  borderWidth: 2
+		}
+	  ]
+	};
+  </script>
+  
+  <Bar {data} />
