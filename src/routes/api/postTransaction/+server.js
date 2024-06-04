19cf1b33f38a3827135ec37583d274a672d64fee
isabellhaid <isabellhaid04@gmail.com>
2024-06-04 13:34:26 +0200

    Update +server.js

--- src/routes/api/postTransaction/+server.js
+++ src/routes/api/postTransaction/+server.js
@@ -2,30 +2,25 @@ import { supabase } from '$lib/supabase';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export async function POST({ request, cookies }) {
-	//extract data from request and cookies:
+	// Extrahiere die Daten aus dem Request und den Cookies
 	const data = await request.json();
 	const session = JSON.parse(cookies.get('session'));
 	const accountList = JSON.parse(cookies.get('account_list'));
 
-	// console.log('session:', session);
-	// console.log('accountList:', accountList);
-
-	//TODO: check if the sender_id corresponds to an account of the logged in user
-
-	//find the account in the accountList that corresponds to the user id of the session:
+	//Finde den Account in der accountList, der der Benutzer-ID der Session entspricht:
 	const account = accountList.find((account) => account.user_id === session.user.id);
 
+	//Überprüfe, ob der sender_id dem Konto des eingeloggten Benutzers entspricht:
 	if (data.sender_id !== account.id) {
 		return new Response('Invalid sender_id', { status: 400 });
 	}
 
-	//check if amount is positive:
+	//Überprüfe, ob der Betrag positiv ist:
 	if (data.amount <= 0) {
 		return new Response('Amount should be greater than 0', { status: 400 });
 	}
 
-	//TODO: perform insert operation and return response to the frontend:
-
+	//Führe die Einfügeoperation durch und gib die Antwort an das Frontend zurück:
 	const response = await fetch(PUBLIC_SUPABASE_URL + '/rest/v1/transactions', {
 		method: 'POST',
 		headers: {
@@ -34,7 +29,7 @@ export async function POST({ request, cookies }) {
 			Authorization: 'Bearer ' + session.access_token
 		},
 		body:
-			//sent data as string:
+			//Sende die Daten als einen JSON-String:
 			JSON.stringify({
 				sender_id: data.sender_id,
 				receiver_id: data.receiver_id,
