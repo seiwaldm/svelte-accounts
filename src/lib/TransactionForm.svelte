030c577ebb759e61208aac272ab569d71e83e72a
rachensp <80815336+rachensp@users.noreply.github.com>
2024-06-05 12:19:19 +0200

    changes

--- src/lib/TransactionForm.svelte
+++ src/lib/TransactionForm.svelte
@@ -44,7 +44,7 @@
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				sender_id: 'db24ee64-5d0c-45ca-9f4a-7a5ca1207124',
+				sender_id: sender,
 				receiver_id: receiverID,
 				amount: amount,
 				purpose: purpose
