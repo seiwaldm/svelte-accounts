c70f14ff01aa67f661c825f95a188e459ddf2108
Michael Ampferer <79844398+Michi1593@users.noreply.github.com>
2024-06-12 11:52:00 +0200

    drawer

---
+++ src/routes/michigabs/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+import { onMount } from 'svelte';
+	import Drawer from '$lib/Drawer.svelte';
+	let drawerOpen = false;
+
+	const toggleDrawer = () => {
+		drawerOpen = !drawerOpen;
+	};
+
+    </script>
+
+<button on:click={toggleDrawer}>Toggle Drawer</button>
+
+<Drawer {isOpen} on:close={() => (drawerOpen = false)}>
+	<h2>Kontenauswahl</h2>
+	<p></p>
+</Drawer>
