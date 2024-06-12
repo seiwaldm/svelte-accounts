72a7ad0cf61db95f1514dec40cb5839e12e451df
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-06-12 12:23:34 +0200

    Revert "Merge branch 'master' of https://github.com/seiwaldm/svelte-accounts"
    This reverts commit 013a2dc7ae9e1e009300c1eb54040d89e85f7e2e, reversing
    changes made to 4b93455f05adc8b6bb1298f80961b970223c5322.

--- src/lib/BalanceLineChart.svelte
+++ src/lib/BalanceLineChart.svelte
@@ -5,14 +5,11 @@
 	import 'chartjs-adapter-date-fns'; // importiert date-fns für chart.js
 	import { onMount } from 'svelte'; // importiert onMount, um Funktionen auszuführen, wenn Komponente gemountet wird
 
-	export let transactions; //exportiert transactions, um sie in App.svelte zu verwenden
+	export let transactions; //exportiert transactions
 
 	//Absteigende Sortierung!
-	let sortedTransactions = transactions.sort(
-		//sortiert transactions nach Datum, absteigend
-		(a, b) => new Date(b.created_at) - new Date(a.created_at) //sortiert nach Datum
-	);
-	let currentBalance = $accounts[0].balance; //setzt aktuellen Kontostand auf Kontostand des ersten Accounts
+	let sortedTransactions = null;
+	let currentBalance = 0; //setzt aktuellen Kontostand auf Kontostand des ersten Accounts
 
 	let calculatedBalance = []; //Array für berechneten Kontostand
 
@@ -26,13 +23,26 @@
 			}
 		}
 	};
+	const chartData = {
+		//Daten für Chart
+		datasets: [
+			//Datensätze
+			{
+				data: calculatedBalance //Daten sind berechneter Kontostand, wird oben berechnet
+			}
+		]
+	};
 
 	onMount(() => {
+		currentBalance = $accounts[0].balance;
+		sortedTransactions = transactions.sort(
+			//sortiert transactions nach Datum, absteigend
+			(a, b) => new Date(b.created_at) - new Date(a.created_at) //sortiert nach Datum
+		);
 		//wird ausgeführt, wenn Komponente gemountet wird (also wenn sie angezeigt wird)
 		sortedTransactions.forEach((transaction) => {
 			//geht alle Transaktionen durch, sortiert nach Datum
 			if (transaction.sender_id === $accounts[0].id) {
-				console.log('sender'); //wenn sender_id des Transactions gleich id des ersten Accounts ist, dann...; wird nur für Testzwecke verwendet
 				currentBalance += transaction.amount; //wir gehen von hinten nach vorne vor, also plus für Ausgaben
 				calculatedBalance.push({
 					//fügt x und y Wert zum Array hinzu
@@ -40,7 +50,7 @@
 					y: currentBalance //y-Wert ist der aktuelle Kontostand
 				});
 				calculatedBalance = calculatedBalance; //setzt Array auf sich selbst, um es zu aktualisieren, da Svelte sonst keine Änderung erkennt
-			} else if (transaction.receiver_id === accounts[0].id) {
+			} else if (transaction.receiver_id === $accounts[0].id) {
 				//wenn receiver_id des Transactions gleich id des ersten Accounts ist, dann...; wird nur für Testzwecke verwendet
 				currentBalance -= transaction.amount; //wir gehen von hinten nach vorne vor, und rechnen deshalb minus für Einnahmen (zurückrechnen)
 				calculatedBalance.push({
@@ -51,19 +61,8 @@
 				calculatedBalance = calculatedBalance;
 			}
 		});
+		document.body.style.zoom = '96%';
 	}); // Close the onMount function
-	chartData.datasets[0].data = calculatedBalance; //setzt Daten des Charts auf berechneten Kontostand, um Chart zu aktualisieren
-
-	const chartData = {
-		//Daten für Chart
-		datasets: [
-			//Datensätze
-			{
-				data: calculatedBalance //Daten sind berechneter Kontostand, wird oben berechnet
-			}
-		]
-	};
 </script>
 
-<Line data={chartData} options={chartOptions} />
-<balanceLineChart transactions={$transactions} />
+<Line id="chart" data={chartData} options={chartOptions} />
