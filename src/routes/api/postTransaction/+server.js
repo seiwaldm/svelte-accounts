862d0e6939c0dec59e50a4eaaed8153695bd7ca2
isabellhaid <isabellhaid04@gmail.com>
2024-06-12 11:45:47 +0200

    j

--- src/routes/api/postTransaction/+server.js
+++ src/routes/api/postTransaction/+server.js
@@ -6,7 +6,14 @@ export async function POST({ request, cookies }) {
 	const data = await request.json();
 	const session = JSON.parse(cookies.get('session'));
 	const accountList = JSON.parse(cookies.get('account_list'));
-	// const account = JSON.parse(cookies.get('account'));
+	const accounts = JSON.parse(cookies.get('accounts'));
+	console.log(accounts);
+
+	//Überprüfe, ob der Kontostand des Senders ausreicht, entnehme die Information von Server
+	if (accounts[0].balance < data.amount) {
+		console.log('Balance too low');
+		return new Response('Balance too low', { status: 400 });
+	}
 
 	// TODO: Namensgebung - 2x account!!!
 	//Finde den Account in der accountList, der der Benutzer-ID der Session entspricht:
@@ -19,12 +26,8 @@ export async function POST({ request, cookies }) {
 
 	//Überprüfe, ob der Betrag positiv ist:
 	if (data.amount <= 0) {
+		console.log('Amount should be greater than 0');
 		return new Response('Amount should be greater than 0', { status: 400 });
-	}
-
-	//Überprüfe, ob der Kontostand des Senders ausreicht:
-	if (account.balance < data.amount) {
-		return new Response('Balance too low', { status: 400 });
 	}
 
 	//Führe die Einfügeoperation durch und gib die Antwort an das Frontend zurück:
