b4a051e56588fc0859ffc0c7fdee0d8bab903933
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-06-15 19:27:23 +0200

    eliminate duplicate const

--- src/routes/api/postTransaction/+server.js
+++ src/routes/api/postTransaction/+server.js
@@ -6,7 +6,7 @@ export async function POST({ request, cookies }) {
 	const data = await request.json();
 	const session = JSON.parse(cookies.get('session'));
 	const accountList = JSON.parse(cookies.get('account_list'));
-	const account = JSON.parse(cookies.get('account'));
+	const accounts = JSON.parse(cookies.get('accounts'));
 
 	//Finde den Account in der accountList, der der Benutzer-ID der Session entspricht:
 	const account = accountList.find((account) => account.user_id === session.user.id);
@@ -45,31 +45,4 @@ export async function POST({ request, cookies }) {
 	});
 	return response;
 
-	//LEGACY CODE:
-	// if ($accounts.map(account => account.id).includes(dummy.sender_id)) {
-	// 	console.log("sender is right")
-	// 	if (dummy.amount <= 0) {
-	// 		return new Response('Amount should be greater than 0', { status: 400 });
-	// 	} else if (dummy.sender_id === accounts[0].id) {
-	// 		if (accounts[0].balance < dummy.amount) {
-	// 			return new Response('balance to low', { status: 400 });
-	// 		}
-	// 	}
-
-	// 	const { data, error } = await supabase.from('transactions').insert([
-	// 		{
-	// 			sender_id: dummy.sender_id,
-	// 			receiver_id: dummy.receiver_id,
-	// 			amount: dummy.amount,
-	// 			purpose: dummy.purpose
-	// 		}
-	// 	]);
-
-	// 	if (error)
-	// 		return new Response(
-	// 			"oh no... seems we have database issues... relax, it's not your fault! plz contact our admin...",
-	// 			{ status: 500 }
-	// 		);
-	// 	return new Response(JSON.stringify(data));
-	// }
 }
