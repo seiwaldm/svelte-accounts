7cb053f9b2114960d7c5d11e0cb49a66259a63a7
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-06-17 07:30:23 +0200

    Update +server.js

--- src/routes/api/postAccount/+server.js
+++ src/routes/api/postAccount/+server.js
@@ -1,22 +1,34 @@
 
-import { supabase } from '$lib/supabase';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export async function POST({ request, cookies }) {
 
+	const data = await request.json();
+	console.log(data)
 	//extract data from request and cookies:
 	const session = JSON.parse(cookies.get("session"));
 
 	// console.log("session:", session);
 	console.log(session.user.id)
 
+
 	//schicke eine Fetch-Anfrage an Supabase
-	fetch(PUBLIC_SUPABASE_URL + "/rest/v1/accounts", {
+	const response = await fetch(PUBLIC_SUPABASE_URL + "/rest/v1/accounts", {
 		method: "POST",
 		headers: {
-			
-		}
+			apikey: PUBLIC_SUPABASE_ANON_KEY,
+			Authorization: "Bearer " + session.access_token,
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(
+			{
+				user_id: session.user.id,
+				designation: data.accountName,
+				balance: data.startingBalance,
+
+			})
+
 	})
 
-	return new Response("ois supa")
+	return response;
 }
